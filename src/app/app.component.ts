fa341960ce4bfd75ed48546e01cddf2a1a2745c7
jatin <jatin.bhatia93@gmail.com>
2018-06-06 00:17:52 +0530

    first commit for tree-table using data from json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  files: any[];
+  cols: any[];
+  selectedRows: any[];
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getFilesystem().then(files => {
+      this.files = files;
+      console.log(this.files);
+    });
+
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      { field: 'size', header: 'Size' },
+      { field: 'type', header: 'Type' }
+  ];
+    
+  }
+
+  nodeSelect(event) {
+    console.log(event)
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {TreeTableModule} from "ng-treetable";
+import { DataService } from './data.service';
 
 
 @NgModule({
@@ -10,9 +12,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    TreeTableModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
